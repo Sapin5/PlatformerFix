8d5a6dcbd4ea90bf2e8b2e0324c9a5de440cc798
Sapin5 <123406040+Sapin5@users.noreply.github.com>
2025-11-09 19:35:04 -0600

    Add Demomap2 and update MapParser for new map
    Added Demomap2.tmj and Demomap2.tmx map files. Updated MapParser.hpp to load Demomap2.tmj, corrected map size assignment, added tileSet loading, and improved drawMap to use dynamic tile size. Project files updated to include Demomap2.tmj.

--- Platformer/MapParser.hpp
+++ Platformer/MapParser.hpp
@@ -12,17 +12,21 @@ private:
 	std::vector<int> mapData;
 	std::vector<int> mapSize;
 	std::vector<int> tileShape;
+	
+	Texture2D tileSet;
 public:
 	/// <summary>
 	/// Load map data from Tiled Json file
 	/// </summary>
 	void LoadMap(const std::string& filePath = " ") {
-		std::ifstream f("Assets/Maps/Demomap.tmj");
+		std::ifstream f("Assets/Maps/Demomap2.tmj");
 		json data = json::parse(f);
 
 		mapData = data.at("layers")[0].at("data").get<std::vector<int>>();
-		mapSize = { data.at("height"), data.at("width") };
+		mapSize = { data.at("width"), data.at("height") };
 		tileShape = { data.at("tilewidth"), data.at("tileheight") };
+
+		tileSet = { LoadTexture("Assets/Maps/Demomap2.tmj") };
 	}
 
 	/// <summary>
@@ -30,16 +34,23 @@ public:
 	/// </summary>
 	void drawMap() {
 		for (int x = 0; x < mapData.size(); x++) {
-			DrawText(TextFormat("%d", mapData[x]), (x % mapSize[1]) * 18, (x / mapSize[1]) * 18, 10, RED);
+			DrawText(TextFormat("%d", mapData[x]), (x % mapSize[0]) * tileShape[0], 
+												   (x / mapSize[0]) * tileShape[1], 10, RED);
 		}
 	}
 
 	/// <summary>
 	/// Return the window size from Tiled json file
 	/// </summary>
-	/// <returns></returns>
+	/// <returns>
+	/// vector { 0, 0 }
+	/// </returns>
 	std::vector<int> getWindowSize() {
 		return mapSize;
+	}
+
+	std::vector<int> getTileShape() {
+		return tileShape;
 	}
 };
 
